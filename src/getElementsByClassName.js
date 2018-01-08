cef7ec1b788ecda33cdf23cf632bd2c6e0fcec4e
Ben Blumenfeld <bmblumenfeld@gmail.com>
2018-01-07 22:59:06 -0800

    getElementsByClassNames runs but this is first draft

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var elementList = [];
+  var traverseDoc = function(node){
+  	if (node.classList.contains(className)){
+  	  elementList.push(node)
+    } 
+  	for (var i = 0; i < node.children.length; i ++){
+  	  traverseDoc(node.children[i]);	
+    }
+  }
+  traverseDoc(document.body);
+  return elementList;
+  
 };
+
+
+// I need to know how to make it work with node.ChildNodes
